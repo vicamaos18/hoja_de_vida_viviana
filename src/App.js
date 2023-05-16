9530c2f248bb133a45666529a0be7f5096edc9b8
vicamaos18 <vicamaos18@gmail.com>
2023-05-16 18:44:18 -0500

    Instalacion de boostrap para importar sus iconos, seccion de contacto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './img.jpeg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { IconName } from "react-icons/ci";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 function TitleSection() {
   return (
@@ -12,19 +13,6 @@ function TitleSection() {
       );  
 }
 
-function ProfilerSection() {
-  return (
-    <div className="ProfilerSection">
-      <div className='SectionLeft'>
-        <TitleSection></TitleSection>
-      </div>
-      <div className='SectionRigth'>
-        <ImageSection></ImageSection>
-      </div>
-    </div>
-      );   
-}
-
 function ImageSection() {
   return (
     <div className="ImageSection">
@@ -32,24 +20,34 @@ function ImageSection() {
         <Card.Img className='ImgProfile' variant="top" src={logo} />
       </Card>
     </div>
+    
     );   
 }
 
 function ContactSection() {
   return (
     <div className="ContactSection">
-      <h2>CONTACTO</h2>
+      <h4>CONTACTO</h4>
       <hr></hr>
-      <p></p>
+      <p><i className="bi bi-envelope-at"></i> vicamaos18@gmail.com</p>
+      <p><i className="bi bi-telephone"></i> 3022704276</p>
+      <p><i className="bi bi-geo-alt"></i> Calle 13 N 31-17 Manizales, Caldas</p>
+      <p><i className="bi bi-instagram"></i> @vivi_nez</p>
+      <p><i className="bi bi-facebook"></i> Vivi Martinez</p>
     </div>
     );   
 }
 
-
 function App() {
   return (
     <div className="App">
-        <ProfilerSection></ProfilerSection>
+      <div className='SectionLeft'>
+        <TitleSection></TitleSection>
+      </div>
+      <div className='SectionRigth'>
+        <ImageSection></ImageSection>
+        <ContactSection></ContactSection>
+      </div>
     </div>
   );
 }
