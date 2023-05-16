0dd103676fb1ee7d6f4639df6a8a599796fa9c85
vicamaos18 <vicamaos18@gmail.com>
2023-05-16 17:48:30 -0500

    creacion componentes, seccion perfil, seccion titulo, seccion imagen, seccion contacto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import logo from './img.jpeg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { IconName } from "react-icons/ci";
+
+function TitleSection() {
+  return (
+    <div className="TitleSection">
+      <p><spam className="Name">Viviana Carolina Martinez Osorio</spam> <br/>Ingeniera de Sistemas y Telecomunicaciones</p>
+    </div>
+      );  
+}
+
+function ProfilerSection() {
+  return (
+    <div className="ProfilerSection">
+      <div className='SectionLeft'>
+        <TitleSection></TitleSection>
+      </div>
+      <div className='SectionRigth'>
+        <ImageSection></ImageSection>
+      </div>
+    </div>
+      );   
+}
+
+function ImageSection() {
+  return (
+    <div className="ImageSection">
+      <Card className='SectionCard'>
+        <Card.Img className='ImgProfile' variant="top" src={logo} />
+      </Card>
+    </div>
+    );   
+}
+
+function ContactSection() {
+  return (
+    <div className="ContactSection">
+      <h2>CONTACTO</h2>
+      <hr></hr>
+      <p></p>
+    </div>
+    );   
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ProfilerSection></ProfilerSection>
     </div>
   );
 }
